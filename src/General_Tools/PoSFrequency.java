a37dca6df4a744ceb34616be3ca4c232bfaa3c6b
Prosper007 <onungwap@gmail.com>
2017-12-10 02:59:32 -0500

    Updated POSFrequency

--- src/General_Tools/PoSFrequency.java
+++ src/General_Tools/PoSFrequency.java
@@ -9,23 +9,17 @@ public class PoSFrequency implements FreqBuilder {
     //Class Variables
     private ArrayList<String> tags;
     private HashMap<String, Integer> pOSFreqMap;
-    private Tagger tagger;
     private int frequency;
 
     public PoSFrequency() throws Exception {
         this.pOSFreqMap = new HashMap<String, Integer>();
-        this.tags = new ArrayList<String>();
-        this.tagger = new Tagger();
     }
 
     //This takes in an ArrayList of tokens, tags them, and computes frequency of tags.
     @Override
     public HashMap<String, Integer> buildFrequencyMap(ArrayList<String> tokens) {
-
-        //first tag the tokens
-        //UNCOMMENT NEXT LINE WHEN TAGGER IS IMPLEMENTED!!!!
-        //this.tags = this.tagger.tag(tokens);
-
+        //Parameter 'tokens' is an ArrayList of 'Tags' which you can get by calling tagger.getTags()!
+        this.tags = new ArrayList<String>(tokens);
         //Determine frequency from tags ArrayList
         for(int i=0; i < tags.size(); i++){
             if(!this.pOSFreqMap.containsKey(tags.get(i))){
@@ -43,9 +37,8 @@ public class PoSFrequency implements FreqBuilder {
     public void printFrequencyMap() {
         for(String POS : this.pOSFreqMap.keySet()){
             this.frequency = this.pOSFreqMap.get(POS);
-        System.out.println("Frequency of "+ POS + "is: " + this.frequency);
-
-    }
+        System.out.println("Frequency of "+ POS + " is: " + this.frequency);
+        }
 }
     //This prints the frequency of just one specified POS
     public void getFrequency(String POS) {
@@ -57,4 +50,13 @@ public class PoSFrequency implements FreqBuilder {
         }
         System.out.println(this.frequency);
     }
+    public static void main(String[] args) throws Exception {
+        PoSFrequency test = new PoSFrequency();
+        Tagger testtagger = new Tagger();
+        testtagger.tagSentence("Saturn is a planet");
+        ArrayList<String> testTags = new ArrayList<>(testtagger.getTags());
+        test.buildFrequencyMap(testTags);
+        test.printFrequencyMap();
+    }
+
 }
