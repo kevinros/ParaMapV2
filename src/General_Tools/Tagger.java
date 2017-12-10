b251094aa2acedb4739d23b1ada92defa5945208
Prosper007 <onungwap@gmail.com>
2017-12-09 19:55:28 -0500

    Updated Tagger

--- src/General_Tools/Tagger.java
+++ src/General_Tools/Tagger.java
@@ -12,31 +12,39 @@ public class Tagger {
 
     private POSModel model;
     private POSTaggerME tag;
+    private POSSample tagsAndSentence;
+    private String SentenceWithTags;
 
     public Tagger() throws Exception {
 
 
         InputStream inputStream = new
-            FileInputStream("src/models/en-pos-maxent.bin");
-        POSModel model = new POSModel(inputStream);
-        POSTaggerME tag = new POSTaggerME(model);
+                FileInputStream("src/models/en-pos-maxent.bin");
+        this.model = new POSModel(inputStream);
+        this.tag = new POSTaggerME(model);
 
     }
 
     //Returned a tagged array from a given sentence
-    public String[]tagSentence(String sentence){
+    public String tagSentence(String sentence) {
         //Tokenizing the sentence using WhitespaceTokenizer class
-        WhitespaceTokenizer whitespaceTokenizer=WhitespaceTokenizer.INSTANCE;
-        String[]tokens=whitespaceTokenizer.tokenize(sentence);
+        WhitespaceTokenizer whitespaceTokenizer = WhitespaceTokenizer.INSTANCE;
+        String[] tokens = whitespaceTokenizer.tokenize(sentence);
 
         //Generating tags
-        String[]tags = tag.tag(tokens);
-        return tags;
+        String[] tags = tag.tag(tokens);
+        this.tagsAndSentence = new POSSample(tokens, tags);
+        this.SentenceWithTags = tagsAndSentence.toString();
+        return SentenceWithTags;
     }
 
+    public static void main(String[] args) throws Exception {
 
 
-
+        Tagger test = new Tagger();
+        String tests = test.tagSentence("Saturn is a planet");
+        System.out.println(tests);
 
 
     }
+}
