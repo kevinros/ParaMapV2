960c3200c8d322f1ccdae10d55de869e1260fc0a
nkuang123 <nkuang@vassar.edu>
2017-12-09 19:43:30 -0500

    Implemented parts of TextSimplifier

--- src/General_Tools/TextSimplifier.java
+++ src/General_Tools/TextSimplifier.java
@@ -1,4 +1,42 @@
 package General_Tools;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class TextSimplifier {
+
+    private HashMap<String, Integer> wordFrequencyMap;
+    private ArrayList<String> taggedSentenceTokens;
+    private String input;
+
+    public TextSimplifier(String input) {
+        this.input = input;
+        this.wordFrequencyMap = new HashMap<>();
+        this.taggedSentenceTokens = new ArrayList<>();
+    }
+
+    private void frequencyMap() throws Exception {
+        WhiteSpaceTokenizer wst = new WhiteSpaceTokenizer();
+        WordFrequency wf = new WordFrequency();
+        ArrayList<String> tokens;
+
+        wst.tokenize(this.input);
+        tokens = wst.getTokens();
+
+        this.wordFrequencyMap = wf.buildFrequencyMap(tokens);
+    }
+
+    public HashMap<String, Integer> getWordFrequencyMap() throws Exception {
+        this.frequencyMap();
+        return this.wordFrequencyMap;
+    }
+
+    private void tagSentences() {
+
+    }
+
+    public ArrayList<String> getTaggedSentenceTokens() {
+        this.tagSentences();
+        return this.taggedSentenceTokens;
+    }
 }
