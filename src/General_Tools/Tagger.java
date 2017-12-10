3b24f424ef0a881ea742e4810d2a63007d9bbf8e
Prosper007 <onungwap@gmail.com>
2017-12-10 03:01:19 -0500

    Updated Tagger

--- src/General_Tools/Tagger.java
+++ src/General_Tools/Tagger.java
@@ -1,6 +1,8 @@
 package General_Tools;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import opennlp.tools.postag.POSModel;
 import opennlp.tools.postag.POSSample;
@@ -14,6 +16,8 @@ public class Tagger {
     private POSTaggerME tag;
     private POSSample tagsAndSentence;
     private String SentenceWithTags;
+    private ArrayList<String> JustTags;
+    private ArrayList<String> JustSentence;
 
     public Tagger() throws Exception {
 
@@ -22,11 +26,13 @@ public class Tagger {
                 FileInputStream("src/models/en-pos-maxent.bin");
         this.model = new POSModel(inputStream);
         this.tag = new POSTaggerME(model);
-
+        this.SentenceWithTags = "";
+        this.JustTags = new ArrayList<>();
+        this.JustSentence = new ArrayList<>();
     }
 
     //Returned a tagged array from a given sentence
-    public String tagSentence(String sentence) {
+    public void tagSentence(String sentence) {
         //Tokenizing the sentence using WhitespaceTokenizer class
         WhitespaceTokenizer whitespaceTokenizer = WhitespaceTokenizer.INSTANCE;
         String[] tokens = whitespaceTokenizer.tokenize(sentence);
@@ -34,16 +40,43 @@ public class Tagger {
         //Generating tags
         String[] tags = tag.tag(tokens);
         this.tagsAndSentence = new POSSample(tokens, tags);
-        this.SentenceWithTags = tagsAndSentence.toString();
-        return SentenceWithTags;
+
+        //Sets the tag field
+        this.JustTags.addAll(Arrays.asList(this.tagsAndSentence.getTags()));
+
+        //Sets the sentence field
+        this.JustSentence.addAll(Arrays.asList(this.tagsAndSentence.getSentence()));
+    }
+
+    public ArrayList<String> getTags(){
+        return this.JustTags;
+    }
+    public ArrayList<String> getSentence(){
+        return this.JustSentence;
+    }
+    public String getSentenceWithTags(){
+        return tagsAndSentence.toString();
     }
 
     public static void main(String[] args) throws Exception {
 
 
         Tagger test = new Tagger();
-        String tests = test.tagSentence("Saturn is a planet");
-        System.out.println(tests);
+        test.tagSentence("Saturn is a planet");
+        String testSentenceAndTags = test.getSentenceWithTags();
+        ArrayList<String> testTags = new ArrayList<>(test.getTags());
+        ArrayList<String> testSentence = new ArrayList<>(test.getSentence());
+
+
+        for(String s : testTags){
+            System.out.println(s);
+        }
+        for(String s : testSentence){
+            System.out.println(s);
+        }
+
+        System.out.println(testSentenceAndTags);
+
 
 
 
