0921775265f72f2d040e2f0b205098d29026ccc7
dalord96 <31749818+dalord96@users.noreply.github.com>
2017-12-11 13:37:12 -0500

    Added Body Tests

--- src/Box_Tools/BodyTest.java
+++ src/Box_Tools/BodyTest.java
@@ -16,6 +16,46 @@ public class BodyTest {
         assertEquals(Description,Test.getBody());
     }
 
+    @Test
+    public void addManyDescriptionsToBody() throws Exception {
+        Body Test = new Body();
+        ArrayList<String> Descriptions = new ArrayList<>(Arrays.asList("12345","678910","11121314"));
+        Test.addManyDescriptionsToBody(Descriptions);
+        assertEquals(Descriptions,Test.getBody());
 
+    }
+
+    @Test
+    public void getBody() throws Exception {
+    }
+
+    @Test
+    public void removeDescriptionFromBody() throws Exception {
+        Body Test = new Body();
+        Test.addDescriptionToBody("12345");
+        Test.removeDescriptionFromBody("12345");
+        ArrayList<String> Description = new ArrayList<>(Arrays.asList(""));
+        assertEquals(Description,Test.getBody());
+
+    }
+
+    @Test
+    public void removeAllDescriptionsFromBody() throws Exception {
+        Body Test = new Body();
+        ArrayList<String> Descriptions = new ArrayList<>(Arrays.asList("12345","678910","11121314"));
+        ArrayList<String> Null = new ArrayList<>(Arrays.asList("12345"));
+        Test.addManyDescriptionsToBody(Descriptions);
+        Test.removeAllDescriptionsFromBody();
+        assertEquals(Null,Test.getBody());
+    }
+
+    @Test
+    public void printBody() throws Exception {
+        Body Test = new Body();
+        Test.addDescriptionToBody("12345");
+        ArrayList<String> Description = new ArrayList<>(Arrays.asList("12345"));
+        assertEquals(Description,Test.getBody());
+
+    }
 
 }
