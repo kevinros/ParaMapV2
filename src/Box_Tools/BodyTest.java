e0731f4ef392122dace4543a0315a56e4b800720
dalord96 <31749818+dalord96@users.noreply.github.com>
2017-12-11 13:36:23 -0500

    Adddescription to Body Test

--- src/Box_Tools/BodyTest.java
+++ src/Box_Tools/BodyTest.java
@@ -2,31 +2,20 @@ package Box_Tools;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class BodyTest {
     @Test
     public void addDescriptionToBody() throws Exception {
+        Body Test = new Body();
+        Test.addDescriptionToBody("12345");
+        ArrayList<String> Description = new ArrayList<>(Arrays.asList("12345"));
+        assertEquals(Description,Test.getBody());
     }
 
-    @Test
-    public void addManyDescriptionsToBody() throws Exception {
-    }
 
-    @Test
-    public void getBody() throws Exception {
-    }
-
-    @Test
-    public void removeDescriptionFromBody() throws Exception {
-    }
-
-    @Test
-    public void removeAllDescriptionsFromBody() throws Exception {
-    }
-
-    @Test
-    public void printBody() throws Exception {
-    }
 
 }
