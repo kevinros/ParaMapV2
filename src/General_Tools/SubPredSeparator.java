31d23f163553d1396ef8484aaa5ed74d65d354bf
Prosper007 <onungwap@gmail.com>
2017-12-11 04:41:05 -0500

    Implemented SupPredSeparator

--- src/General_Tools/SubPredSeparator.java
+++ src/General_Tools/SubPredSeparator.java
@@ -1,39 +1,92 @@
 package General_Tools;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
-public class SubPredSeparator {
+public class SubPredSeparator{
     //Class Variables
-    private HashMap<String, ArrayList<String>> HeadToBod;
+    private HashMap<String, ArrayList<ArrayList<String>>> HeadToBod;
     private String head;
     private ArrayList<String> body;
-    private WhiteSpaceTokenizer tokenizer;
+    private Tagger tagger;
+    private ArrayList<ArrayList<String>> listofListofSentences;
+    private ArrayList<ArrayList<String>> listOfListOfTags;
 
     public SubPredSeparator() throws Exception{
         this.head = "";
-        this.body = new ArrayList<String>();
-        this.HeadToBod = new HashMap<String, ArrayList<String>>();
-        tokenizer = new WhiteSpaceTokenizer();
+        this.body = new ArrayList<>();
+        this.HeadToBod = new HashMap<>();
+        this.tagger = new Tagger();
+        this.listofListofSentences = new ArrayList<>();
+        this.listOfListOfTags = new ArrayList<>();
     }
 
-    public void setHead(String sentence) {
-        tokenizer.tokenize(sentence);
-        ArrayList<String> words = new ArrayList<>(tokenizer.getTokens());
-        this.head = words.get(0);
+    public void setHead(ArrayList<String> sentences) {
+        //Adds sentences and tags to arrayLists
+        createLists(sentences);
+
+        //Set Head and Body and creates map in process
+        setHeadandBod();
     }
 
-    public void setBody(String sentence) {
-        tokenizer.tokenize(sentence);
-        ArrayList<String> words = new ArrayList<>(tokenizer.getTokens());
-        words.remove(0);
-        this.body = words;
+    //Adds sentences and tags to arraylists
+    private void createLists(ArrayList<String> sentences){
+        for(String s : sentences){
+            this.tagger.tagSentence(s);
+            this.listofListofSentences.add(this.tagger.getSentence());
+            this.listOfListOfTags.add(this.tagger.getTags());
+            this.tagger.clear();
+        }
     }
 
-    public void createMap(String sentence) {
-        setHead(sentence);
-        setBody(sentence);
-        this.HeadToBod.put(this.head, this.body);
+    //Sets the head and body
+    private void setHeadandBod() {
+        //List of all possible subjects
+        ArrayList<String> subjectList = new ArrayList<>(Arrays.asList("NN", "NNS", "NNP", "NNPS"));
+        //iterate through ArrayList of ArrayList of listOfListOfTags
+        for (int i = 0; i < this.listOfListOfTags.size(); i++) {
+            //ith ArrayList of Tags to iterate through
+            ArrayList<String> listOfTags = this.listOfListOfTags.get(i);
+            //Corresponding ith ArrayList of Sentences to extract head and body from
+            ArrayList<String> listOfSentences = this.listofListofSentences.get(i);
+
+            //Iterate through ith ArrayList of Tags
+            for (int j = 0; j < listOfTags.size(); j++) {
+                //jth tag to inspect
+                String comparedTag = listOfTags.get(j);
+
+                //Check if the tag is a subject tag
+                if (subjectList.contains(comparedTag)) {
+                    this.head = listOfSentences.get(j);
+                    //iterate through rest of sentence and add to body
+                    for (int k = j + 1; k < listOfSentences.size() - j - 1; k++) {
+                        this.body.add(listOfSentences.get(k));
+                    }
+                    break;
+                }
+            }
+            createMap();
+            clear();
+
+
+        }
+    }
+
+
+
+    private void createMap() {
+        if (this.HeadToBod.containsKey(this.head))
+            this.HeadToBod.get(this.head).add(this.body);
+        else {
+            this.HeadToBod.put(this.head, new ArrayList<>());
+            this.HeadToBod.get(this.head).add(this.body);
+        }
+    }
+
+    private void clear(){
+        this.head = "";
+        this.body.clear();
     }
 
     public String getHead() {
@@ -44,15 +97,13 @@ public class SubPredSeparator {
         return this.body;
     }
 
-    public HashMap<String, ArrayList<String>> getMap() {
+    public HashMap<String, ArrayList<ArrayList<String>>> getMap() {
         return this.HeadToBod;
     }
 
 
     public static void main(String[] args) throws Exception {
         SubPredSeparator test = new SubPredSeparator();
-
-        test.createMap("Saturn is a planet");
 
         String testHead = test.getHead();
         System.out.println(testHead);
@@ -67,12 +118,12 @@ public class SubPredSeparator {
         String body = "";
         for(String head : test.HeadToBod.keySet()) {
             System.out.println("Head of sentence is: " + head);
-            for (String bod : test.HeadToBod.get(head)) {
+           /* for (String bod : test.HeadToBod.get(head)) {
                 body += bod + " ";
-            }
+           */ }
             System.out.println("Body of sentence is: " +body);
         }
 
     }
-}
+
 
