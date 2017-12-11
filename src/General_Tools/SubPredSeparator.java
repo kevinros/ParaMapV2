c944e443c666144b5526aaea3715e9c73f638f76
Prosper007 <onungwap@gmail.com>
2017-12-11 09:18:06 -0500

    Implemented SubPredSeparator

--- src/General_Tools/SubPredSeparator.java
+++ src/General_Tools/SubPredSeparator.java
@@ -22,28 +22,31 @@ public class SubPredSeparator{
         this.listOfListOfTags = new ArrayList<>();
     }
 
-    public void setHead(ArrayList<String> sentences) {
+    public void doALot(ArrayList<String> sentences) {
         //Adds sentences and tags to arrayLists
         createLists(sentences);
 
-        //Set Head and Body and creates map in process
+        //Sets Head and Body and creates map in process
         setHeadandBod();
     }
 
-    //Adds sentences and tags to arraylists
+    //Adds sentences and tags to arrayLists
     private void createLists(ArrayList<String> sentences){
         for(String s : sentences){
             this.tagger.tagSentence(s);
-            this.listofListofSentences.add(this.tagger.getSentence());
-            this.listOfListOfTags.add(this.tagger.getTags());
+            ArrayList<String> sent = new ArrayList<>(this.tagger.getSentence());
+            ArrayList<String> tagz = new ArrayList<>(this.tagger.getTags());
+            this.listofListofSentences.add(sent);
+            this.listOfListOfTags.add(tagz);
             this.tagger.clear();
         }
     }
 
-    //Sets the head and body
+    //Sets the head and body and creates map in process
     private void setHeadandBod() {
         //List of all possible subjects
         ArrayList<String> subjectList = new ArrayList<>(Arrays.asList("NN", "NNS", "NNP", "NNPS"));
+        ArrayList<String> pluralList = new ArrayList<>(Arrays.asList("NNS", "NNPS"));
         //iterate through ArrayList of ArrayList of listOfListOfTags
         for (int i = 0; i < this.listOfListOfTags.size(); i++) {
             //ith ArrayList of Tags to iterate through
@@ -58,43 +61,65 @@ public class SubPredSeparator{
 
                 //Check if the tag is a subject tag
                 if (subjectList.contains(comparedTag)) {
-                    this.head = listOfSentences.get(j);
+                    if(pluralList.contains(comparedTag)){
+                        //if plurar, convert to singular first
+                        this.head = convertToSingular(listOfSentences.get(j));
+                    }
+                    else {
+                        this.head = listOfSentences.get(j);
+                    }
                     //iterate through rest of sentence and add to body
-                    for (int k = j + 1; k < listOfSentences.size() - j - 1; k++) {
+                    for (int k = j+1; k < listOfSentences.size(); k++) {
                         this.body.add(listOfSentences.get(k));
                     }
                     break;
                 }
             }
+            //creates hashmap and checks if head already exists. If so, it merges heads.
             createMap();
+            //Clean slate for next iteration
             clear();
-
-
         }
     }
 
 
-
+    //creates hashmap and checks if head already exists. If so, it merges heads.
     private void createMap() {
-        if (this.HeadToBod.containsKey(this.head))
-            this.HeadToBod.get(this.head).add(this.body);
+        String head = this.head.toLowerCase();
+        ArrayList<String> newbie = new ArrayList<>(this.body);
+        if (this.HeadToBod.containsKey(head))
+            this.HeadToBod.get(head).add(newbie);
         else {
-            this.HeadToBod.put(this.head, new ArrayList<>());
-            this.HeadToBod.get(this.head).add(this.body);
+            this.HeadToBod.put(head, new ArrayList<>());
+            this.HeadToBod.get(head).add(newbie);
         }
     }
-
+    //Allows class reusability
     private void clear(){
         this.head = "";
         this.body.clear();
     }
 
-    public String getHead() {
-        return this.head;
-    }
+    //Converts common plurals to singular
+    private String convertToSingular(String word) {
+        String singular = "";
+        if (word.charAt(word.length() - 1) == 's') {
+            if (word.charAt(word.length() - 2) == 'e') {
+                if (word.charAt(word.length() - 3) == 'i') {
+                    singular = word.substring(0, word.length() - 3) + "y";
+                } else {
+                    singular = word.substring(0, word.length() - 2);
+                }
+            }
+            else {
+                singular = word.substring(0, word.length()-1);
+            }
+        }
+        else if ((word.charAt(word.length() - 1) == 'i')) {
+            singular = word.substring(0, word.length()-1) + "us";
+        }
+        return singular;
 
-    public ArrayList<String> getBody() {
-        return this.body;
     }
 
     public HashMap<String, ArrayList<ArrayList<String>>> getMap() {
@@ -104,24 +129,24 @@ public class SubPredSeparator{
 
     public static void main(String[] args) throws Exception {
         SubPredSeparator test = new SubPredSeparator();
+        ArrayList<String> testSentences = new ArrayList<>(Arrays.asList("A class is horrible", " A Planet is huge", "Saturn is next to Uranus", "Planets may explode", "Classes suck"));
 
-        String testHead = test.getHead();
-        System.out.println(testHead);
-
-        ArrayList<String> testBody =  new ArrayList<>(test.getBody());
-        for (String bod : testBody) {
-            System.out.print(bod + " ");
-        }
-
-        System.out.println();
+        test.doALot(testSentences);
+        HashMap<String, ArrayList<ArrayList<String>>> testMap = new HashMap<>(test.getMap());
+        System.out.println(testMap.keySet());
 
         String body = "";
-        for(String head : test.HeadToBod.keySet()) {
+        for(String head : testMap.keySet()) {
             System.out.println("Head of sentence is: " + head);
-           /* for (String bod : test.HeadToBod.get(head)) {
-                body += bod + " ";
-           */ }
-            System.out.println("Body of sentence is: " +body);
+           for (ArrayList<String> bods : testMap.get(head)) {
+               for(String s : bods) {
+                   body += s + " ";
+               }
+               System.out.println("Body of sentence is: " +body);
+               body = "";
+           }
+            System.out.println();
+            }
         }
 
     }
