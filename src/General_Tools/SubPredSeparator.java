cdd1ad04311066b56aae2702d96eb3a8b542c997
Prosper007 <onungwap@gmail.com>
2017-12-11 20:01:45 -0500

    Updated SubPredSeparator

--- src/General_Tools/SubPredSeparator.java
+++ src/General_Tools/SubPredSeparator.java
@@ -22,28 +22,11 @@ public class SubPredSeparator{
         this.listOfListOfTags = new ArrayList<>();
     }
 
-    public void doALot(ArrayList<String> sentences) {
+    //Creates map of head to body from a given list of sentences
+    public void createMap(ArrayList<String> sentences) {
         //Adds sentences and tags to arrayLists
         createLists(sentences);
 
-        //Sets Head and Body and creates map in process
-        setHeadandBod();
-    }
-
-    //Adds sentences and tags to arrayLists
-    private void createLists(ArrayList<String> sentences){
-        for(String s : sentences){
-            this.tagger.tagSentence(s);
-            ArrayList<String> sent = new ArrayList<>(this.tagger.getSentence());
-            ArrayList<String> tagz = new ArrayList<>(this.tagger.getTags());
-            this.listofListofSentences.add(sent);
-            this.listOfListOfTags.add(tagz);
-            this.tagger.clear();
-        }
-    }
-
-    //Sets the head and body and creates map in process
-    private void setHeadandBod() {
         //List of all possible subjects
         ArrayList<String> subjectList = new ArrayList<>(Arrays.asList("NN", "NNS", "NNP", "NNPS"));
         ArrayList<String> pluralList = new ArrayList<>(Arrays.asList("NNS", "NNPS"));
@@ -75,16 +58,29 @@ public class SubPredSeparator{
                     break;
                 }
             }
-            //creates hashmap and checks if head already exists. If so, it merges heads.
-            createMap();
+            //Adds the current head and body to the map. Checks if head already exists. If it does, it merges
+            // it with new body
+            addToMap();
             //Clean slate for next iteration
             clear();
         }
     }
 
+    //Adds sentences and tags to arrayLists
+    private void createLists(ArrayList<String> sentences){
+        for(String s : sentences){
+            this.tagger.tagSentence(s);
+            ArrayList<String> sent = new ArrayList<>(this.tagger.getSentence());
+            ArrayList<String> tagz = new ArrayList<>(this.tagger.getTags());
+            this.listofListofSentences.add(sent);
+            this.listOfListOfTags.add(tagz);
+            this.tagger.clear();
+        }
+    }
 
-    //creates hashmap and checks if head already exists. If so, it merges heads.
-    private void createMap() {
+
+    //Adds the current head and body to the map. Checks if head already exists. If it does, it merges it with new body
+    private void addToMap() {
         String head = this.head.toLowerCase();
         ArrayList<String> newbie = new ArrayList<>(this.body);
         if (this.HeadToBod.containsKey(head))
@@ -126,26 +122,29 @@ public class SubPredSeparator{
         return this.HeadToBod;
     }
 
+    public void printMap(){
+        String body = "";
+        for(String head : this.HeadToBod.keySet()) {
+            System.out.println("Head of sentence is: " + head);
+            for (ArrayList<String> bods : this.HeadToBod.get(head)) {
+                for(String s : bods) {
+                    body += s + " ";
+                }
+                System.out.println("Body of sentence is: " +body);
+                body = "";
+            }
+            System.out.println();
+        }
+    }
+
 
     public static void main(String[] args) throws Exception {
         SubPredSeparator test = new SubPredSeparator();
-        ArrayList<String> testSentences = new ArrayList<>(Arrays.asList("A class is horrible", " A Planet is huge", "Saturn is next to Uranus", "Planets may explode", "Classes suck"));
+        ArrayList<String> testSentences = new ArrayList<>(Arrays.asList("A class is horrible", " A Planet is huge",
+                "Saturn is next to Uranus", "Planets may explode", "Classes suck"));
 
-        test.doALot(testSentences);
-        HashMap<String, ArrayList<ArrayList<String>>> testMap = new HashMap<>(test.getMap());
-        System.out.println(testMap.keySet());
-        String body = "";
-        for(String head : testMap.keySet()) {
-            System.out.println("Head of sentence is: " + head);
-           for (ArrayList<String> bods : testMap.get(head)) {
-               for(String s : bods) {
-                   body += s + " ";
-               }
-               System.out.println("Body of sentence is: " +body);
-               body = "";
-           }
-            System.out.println();
-            }
+        test.createMap(testSentences);
+        test.printMap();
         }
 
     }
