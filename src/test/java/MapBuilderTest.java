5ebd4f32d9c1101ece74aaf3a47d27c380d9a72e
Kevin Ros <kevinros1190@gmail.com>
2017-12-15 00:17:10 -0500

    test

--- src/test/java/MapBuilderTest.java
+++ src/test/java/MapBuilderTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.*;
 public class MapBuilderTest {
     @Test
     public void buildMap() throws Exception {
+        String userInput = "Saturn is a planet. Saturn has rings. Billy has rings. Billy's cat is blue.";
+
     }
 
 }
