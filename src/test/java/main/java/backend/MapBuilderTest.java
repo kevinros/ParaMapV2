c4d8e0e8d6dd8e7d81671f1f04e1c31936a4b728
Kevin Ros <kevinros1190@gmail.com>
2017-12-14 22:38:13 -0500

    only testing public methods

---
+++ src/test/java/main/java/backend/MapBuilderTest.java
@@ -0,0 +1,12 @@
+package main.java.backend;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MapBuilderTest {
+    @Test
+    public void buildMap() throws Exception {
+    }
+
+}
