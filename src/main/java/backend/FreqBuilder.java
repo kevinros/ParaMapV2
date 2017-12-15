7452a07f4466ae4faa5b8b0810e79a9755d35d0b
Kevin Ros <kevinros1190@gmail.com>
2017-12-15 00:46:22 -0500

    added javadoc description for FreqBuilder Class

--- src/main/java/backend/FreqBuilder.java
+++ src/main/java/backend/FreqBuilder.java
@@ -3,9 +3,22 @@ package main.java.backend;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * Every type of frequency builder will have these methods.
+ */
 public interface FreqBuilder {
 
+    /**
+     * Makes hashmap between Strings and Integers
+     * to count frequency of something. 
+     * @param tokens tokenized sentence.
+     * @return HashMap of string to int.
+     */
     public HashMap<String, Integer> buildFrequencyMap(ArrayList<String> tokens);
+
+    /**
+     * Prints the hashmap of strings to integers.
+     */
     public void printFrequencyMap();
 }
 
